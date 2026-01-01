1716975442aa63f5ae522b20a0f9137a95381a7c
Tanishka <tanishkagupta607@gmail.com>
2026-01-02 00:20:16 +0530

    Day 1: Best time to sell and buy stocks

---
+++ Arrays/buysellstocks.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number of days: ";
+    cin >> n;
+
+    vector<int> prices(n);
+    cout << "Enter stock prices: ";
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    Solution sol;
+    int result = sol.maxProfit(prices);
+
+    cout << "Maximum Profit: " << result << endl;
+
+    return 0;
+}
